f800480c472eec2cda504812c3779897ca4dc6e3
Васильева Анна <ajvvshi@gmail.com>
2025-10-13 14:14:29 +0500

    model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 class Todo(BaseModel):
     id: int
@@ -9,5 +10,28 @@ class Todo(BaseModel):
             "example": {
                 "id": 1,
                 "item": "First Todo is to finish this book!"
+            }
+        }
+class TodoItem(BaseModel):
+    item: str
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
             }
         }
