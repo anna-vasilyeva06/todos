467977716ec3dea2c1e50d92b9bac125926e19c0
Васильева Анна <ajvvshi@gmail.com>
2025-11-24 13:46:22 +0500

    with SQL

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,21 +1,44 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+
+class Event(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
 
-    class Config:
-        schema_extra = {
+    model_config = {
+        "arbitrary_types_allowed": True,
+        "json_schema_extra": {
             "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
                 "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet"
             }
         }
+    }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str] = None
+    image: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
+    location: Optional[str] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+    }
--- planner/routes/users.py
+++ planner/routes/users.py
@@ -22,20 +22,16 @@ async def sign_new_user(data: NewUser) -> dict:
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
-    # Исправленная логика
     if user.email not in users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User does not exist"
         )
-
-    # Проверка пароля (вынесена из блока if)
     if users[user.email].password != user.password:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Wrong credentials passed"
         )
-
     return {
         "message": "User signed in successfully"
     }
